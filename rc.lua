6e91551a76dd410411735c58201fa364c01b18dd
jllado <jllado@gmail.com>
2021-07-07 09:04:27 +0200

    Added alacritty as terminal

--- rc.lua
+++ rc.lua
@@ -66,7 +66,7 @@ end
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init("~/.config/awesome/darktheme.lua")
 -- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
